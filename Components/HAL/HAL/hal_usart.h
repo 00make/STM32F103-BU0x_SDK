dd78537d059401840c0e7638f027a7869ba803f1
00make <towardsrwby@gmail.com>
2025-01-15 18:22:24 +0800

    refactor: 优化 hal_usart.h 文件，清理多余空行并统一格式

--- Components/HAL/HAL/hal_usart.h
+++ Components/HAL/HAL/hal_usart.h
@@ -6,7 +6,6 @@ extern "C"
 {
 #endif
 
-
 /**************************************************************************************************
  *                                                                              INCLUDES
  **************************************************************************************************/
@@ -14,65 +13,58 @@ extern "C"
 #include "OSAL_Comdef.h"
 #include "stm32f10x.h"
 
-
-
 /**************************************************************************************************
  *                                                                              CONSTANTS
  **************************************************************************************************/
-#define HAL_USART1                  /*调试口*/
-#define HAL_USART2                  /*透传口*/
+#define HAL_USART1 /*调试口*/
+#define HAL_USART2 /*透传口*/
 
 #define HAL_USART1_DMA
 #define HAL_USART2_DMA
 
+// LED端口定义
+#define HAL_USART1_RCC RCC_APB2Periph_USART1 | RCC_APB2Periph_GPIOA
+#define HAL_USART1_PORT GPIOA
+#define HAL_USART1_TXD_PIN GPIO_Pin_9
+#define HAL_USART1_RXD_PIN GPIO_Pin_10
+#define HAL_USART1_SN USART1
 
-//LED端口定义
-#define HAL_USART1_RCC          RCC_APB2Periph_USART1 | RCC_APB2Periph_GPIOA
-#define HAL_USART1_PORT         GPIOA
-#define HAL_USART1_TXD_PIN      GPIO_Pin_9
-#define HAL_USART1_RXD_PIN      GPIO_Pin_10
-#define HAL_USART1_SN           USART1
+    /***************************************************************************************************
+     *                                                                              TYPEDEF
+     ***************************************************************************************************/
+    typedef void (*halUARTCBack_t)(uint8_t port, uint8_t event);
 
+    /***************************************************************************************************
+     *                                                                              GLOBAL VARIABLES
+     ***************************************************************************************************/
+    extern char USART1_DMA_RX_BYTE;
+    extern char USART2_DMA_RX_BYTE;
 
+    /**************************************************************************************************
+     *                                        FUNCTIONS - API
+     **************************************************************************************************/
+    extern void _dbg_printf(const char *format, ...);
 
-/***************************************************************************************************
- *                                                                              TYPEDEF
- ***************************************************************************************************/
-typedef void (*halUARTCBack_t) (uint8_t port, uint8_t event);
+    extern uint16_t USART1_SendBuffer(const char *buffer, uint16_t length, int flag);
 
+    extern void USART2SendDatas(unsigned char *str, char len);
 
-/***************************************************************************************************
- *                                                                              GLOBAL VARIABLES
- ***************************************************************************************************/
-extern char USART1_DMA_RX_BYTE;
-extern char USART2_DMA_RX_BYTE;
+    extern void HalUARTInit(void);
 
+    static void HalUASRT1_NVIC_Config(void);
 
-/**************************************************************************************************
- *                                        FUNCTIONS - API
- **************************************************************************************************/
-extern void _dbg_printf (const char* format, ...);
+    static void HalUASRT2_NVIC_Config(void);
 
-extern uint16_t USART1_SendBuffer (const char* buffer, uint16_t length, int flag);
+    static void HalUSART1_Init(u32 bound);
 
-extern void USART2SendDatas (unsigned char* str, char len);
+    static void HalUSART2_Init(void);
 
-extern void HalUARTInit (void);
+    static void HalUSART1_IO_Init(void);
 
-static void HalUASRT1_NVIC_Config (void);
-
-static void HalUASRT2_NVIC_Config (void);
-
-static void HalUSART1_Init (u32 bound);
-
-static void HalUSART2_Init (void);
-
-static void HalUSART1_IO_Init (void);
-
-static void HalUSART2_IO_Init (void);
+    static void HalUSART2_IO_Init(void);
 
 #ifdef __cplusplus
 }
 #endif
 
-#endif  //__HAL_UART_H
+#endif //__HAL_UART_H
